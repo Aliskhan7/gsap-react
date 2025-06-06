ad6dde1e08b589267f0bde5e4a8bc3ed91b187f0
aliskhangazamatov <aliskhanios@gmail.com>
2025-06-06 22:45:21 +0300

    animate scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,44 @@
+import React, {  useRef } from 'react';
 import gsap from 'gsap';
-import {useGSAP} from "@gsap/react";
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { useGSAP } from '@gsap/react';
 
-function App() {
-    gsap.registerPlugin(useGSAP)
+gsap.registerPlugin(useGSAP, ScrollTrigger);
 
+export default function Scroll() {
+    const main = useRef();
 
-        const { contextSafe } = useGSAP();
+    useGSAP(
+        () => {
+            const boxes = gsap.utils.toArray('.box');
+            boxes.forEach((box) => {
+                gsap.to(box, {
+                    x: 150,
+                    scrollTrigger: {
+                        trigger: box,
+                        start: 'bottom bottom',
+                        end: 'top 20%',
+                        scrub: true,
+                        // markers: true,
+                    },
+                });
+            });
+        },
+        { scope: main }
+    );
 
-        // ✅ wrapped in contextSafe() - animation will be cleaned up correctly
-        const onEnter = contextSafe(({ currentTarget }) => {
-            gsap.to(currentTarget, { rotation: "+=360" });
-        });
-  return (
-      <div className='class="panel center text-center"'>
-
-
-      <div className="app flex-row">
-          <div className="box gradient-blue" onClick={onEnter}>
-              Click Me
-          </div>
-      </div>
-      </div>
-  )
+    return (
+        <div>
+            <section className="section flex-center column">
+                <h2>Basic ScrollTrigger with React</h2>
+                <p>Scroll down to see the magic happen!!</p>
+            </section>
+            <div className="section flex-center column" ref={main}>
+                <div className="box gradient-blue">box</div>
+                <div className="box gradient-blue">box</div>
+                <div className="box gradient-blue">box</div>
+            </div>
+            <section className="section"></section>
+        </div>
+    );
 }
-
-export default App
